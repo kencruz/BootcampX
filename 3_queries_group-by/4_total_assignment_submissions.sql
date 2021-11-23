71d99d68cebf0577c0e40ca5925bb8b345ec43ff
Ken Cruz <ken@kencruz.ca>
2021-11-22 21:26:53 -0500

    added 3_queries_group-by

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+GROUP BY cohort
+ORDER BY count(assignment_submissions.*) DESC;
+

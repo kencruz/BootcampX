71f8344e67ca5ac5cf46061cc459c52fb6cd67d1
Ken Cruz <ken@kencruz.ca>
2021-11-24 22:45:49 -0500

    used parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,6 @@
 const [cohortQuery, limit] = process.argv.slice(2);
 const { Pool } = require('pg');
+const values = [cohortQuery, limit];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,13 +9,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortQuery}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const values = [cohortQuery];
+
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortQuery}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
